0c31eacd684b957a612fba4944c802eaceae4ca6
Irie Aoi <eai@mizle.net>
2020-09-17 11:39:21 +0900

    タイトルシーン

---
+++ Assets/Title.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Title : MonoBehaviour
+{
+    private bool firstPush = false;
+    public void PressStart()
+    {
+        Debug.Log("Press Start!");
+        if (!firstPush) {
+            Debug.Log("Go Next Scene!");
+            //ここに次のシーンへいく命令を書く
+
+            //
+            firstPush = true;
+        }
+    }
+}
